cdfc145fd3b36de0b0c3d3452ff899f0df67db1b
Sahana Hariharan <sahanahariharan@gmail.com>
2026-09-21 15:47:02 -0700

    Fix installation command for human-eval package

--- MP1/setup_dataset.sh
+++ MP1/setup_dataset.sh
@@ -1,6 +1,6 @@
 pip3 install datasets==2.16.1
 git clone https://github.com/openai/human-eval
-pip3 install -e human-eval
+pip3 install ./human-eval
 sed -i 's/^#\([[:space:]]*\)exec(check_program, exec_globals)/\1exec(check_program, exec_globals)/' human-eval/human_eval/execution.py
 sed -i '/assert len(completion_id) == len(problems), "Some problems are not attempted."/s/^/#/' human-eval/human_eval/evaluation.py
 
