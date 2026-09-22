f2ca49443ba211f09ae604596e85c0c48498418a
Sahana Hariharan <sahanahariharan@gmail.com>
2026-09-21 17:00:46 -0700

    Remove load_in_4bit from model loading

--- MP1/model_prompting.py
+++ MP1/model_prompting.py
@@ -22,7 +22,6 @@ def prompt_model(dataset, model_name = "deepseek-ai/deepseek-coder-6.7b-base", q
         # TODO: load the model with quantization
         model = AutoModelForCausalLM.from_pretrained(
             model_name,
-            load_in_4bit=True,
             device_map='auto',
             torch_dtype=torch.bfloat16,
             quantization_config=BitsAndBytesConfig(
